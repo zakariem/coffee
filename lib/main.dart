7a7ccda6fe6481f3cba28ff6620a69a52239cb59
zakariem <zakariyem367@gmail.com>
2025-01-10 12:28:18 +0300

    Remove Firebase configuration and related files; update AndroidManifest and Gradle files for cleaner setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
-
 import 'package:coffee_shop/page/onboard_page.dart';
 import 'package:flutter/material.dart';
 
-// import 'page/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
-    return runApp(
+// await Firebase.initializeApp(
+//   options: DefaultFirebaseOptions.currentPlatform,
+// );
+  return runApp(
     ProviderScope(
       child: MyApp(),
     ),
@@ -24,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  OnboardPage(),
+      home: OnboardPage(),
     );
   }
 }
